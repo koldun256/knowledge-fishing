9073d0bfe8e4fb82f3b8115959d2104100c85bbd
Даниил Шевнин <shevnin.dm@phystech.edu>
2025-10-02 00:57:08 +0300

    change database url

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ const getBaseUrl = () => {
   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
     return 'http://localhost:8000';
   }
-  return 'http://141.8.198.111:8000/';
+  return 'http://141.8.198.111:8000';
 };
 
 export const API_CONFIG = {
