0f156add7b2cf59e4c77c84e4d4ec441919a3088
Shevnin Daniil <shevnin.dm@phystech.edu>
2026-01-22 21:43:45 +0300

    fix indent in create pond type modal

--- src/components/CreatePondModal.jsx
+++ src/components/CreatePondModal.jsx
@@ -327,12 +327,12 @@ export default function CreatePondModal({ isOpen, onClose, onCreate, userPonds =
 
   return ReactDOM.createPortal(
     <div 
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[10000] p-4"
+      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[10000] p-0"
       onClick={handleBackdropClick}
     >
       <style>{webkitSpinButtonStyles}</style>
       
-      <div className="bg-white p-6 rounded-xl w-[90%] max-w-[500px] max-h-[90vh] shadow-2xl overflow-hidden flex flex-col relative">
+      <div className="bg-white p-4 xs:p-8 rounded-xl w-[90%] max-w-[500px] max-h-[90vh] shadow-2xl overflow-hidden flex flex-col relative">
         <button
           onClick={handleClose}
           className="absolute top-4 right-4 bg-transparent border-none text-2xl cursor-pointer text-gray-600 w-8 h-8 flex items-center justify-center rounded transition-all duration-300 ease-in-out z-10 hover:bg-gray-100 hover:text-gray-800"
